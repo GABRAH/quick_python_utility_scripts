af92de73b50f301cfe34e2f6157f46364403f831
GABRAH <hb1115@york.ac.uk>
2022-01-25 22:48:45 +0000

    Wrote func to import csv file and query API, now need to combine all that

--- find_models_for_uniprotID/main.py
+++ find_models_for_uniprotID/main.py
@@ -1,6 +1,7 @@
 import os
 import re
 import sys
+import csv
 import argparse
 import requests
 import warnings
@@ -30,6 +31,7 @@ def download_and_prepare_alphafoldDB_model(uniprotID, downloadLocation):
 
 
 def query_uniprotID(uniprotID):
+    sanitised_PDBentries = []
     uniprotRequestURL = f"https://www.ebi.ac.uk/proteins/api/proteins/{uniprotID}"
     uniprotResponse = requests.get(
         uniprotRequestURL, headers={"Accept": "application/json"}
@@ -45,22 +47,86 @@ def query_uniprotID(uniprotID):
     PDBentries = [element for element in dbReferences if element['type'] == "PDB"]
     PDBentries_NMR = [element for element in PDBentries if element['properties']['method'] == "NMR"]
     PDBentries_XrayCryoEM = [element for element in PDBentries if element['properties']['method'] == "X-ray" or element['properties']['method'] == "EM"]
-    # need to convert here resolution field to float by removing amstrong symbol
-    PDBentries_XrayCryoEM_sorted = PDBentries_XrayCryoEM.sort(key=lambda e: float(e['properties']['resolution']), reverse=True)
-    for PDBentry in PDBentries_XrayCryoEM_sorted:
-        print(PDBentry)
+    for index, item in enumerate(PDBentries_XrayCryoEM):
+        oldString = item['properties']['resolution']
+        newString = oldString.replace(' A', '')
+        convertedFloat = float(newString)
+        PDBentries_XrayCryoEM[index]['properties']['resolution'] = convertedFloat
+    PDBentries_XrayCryoEM.sort(key=lambda e: e['properties']['resolution'], reverse=False)
+    for PDBentry in PDBentries_XrayCryoEM:
+        modelID = PDBentry['id']
+        modelMethod = PDBentry['properties']['method']
+        modelResolution = PDBentry['properties']['resolution']
+        chainInfo = PDBentry['properties']['chains']
+        modelChains = chainInfo.split("=", 1)[0]
+        modelChainRange = chainInfo.split("=", 1)[1]
+        modelChainStart = int(modelChainRange.split("-")[0])
+        modelChainEnd = int(modelChainRange.split("-")[1])
+        entryToAppend = {"id": modelID, "method": modelMethod, "resolution": modelResolution, "chains": modelChains, "chain_start": modelChainStart, "chain_end": modelChainEnd}
+        sanitised_PDBentries.append(entryToAppend)
     
-    print(outputSequence)
-    print(outputSequenceLength)
-    output = {
-        "sequenceLength": outputSequenceLength,
-        "sequence": outputSequence,
-    }
+    for PDBentry in PDBentries_NMR:
+        modelID = PDBentry['id']
+        modelMethod = PDBentry['properties']['method']
+        modelResolution = 0
+        chainInfo = PDBentry['properties']['chains']
+        modelChains = chainInfo.split("=", 1)[0]
+        modelChainRange = chainInfo.split("=", 1)[1]
+        modelChainStart = int(modelChainRange.split("-")[0])
+        modelChainEnd = int(modelChainRange.split("-")[1])
+        entryToAppend = {"id": modelID, "method": modelMethod, "resolution": modelResolution, "chains": modelChains, "chain_start": modelChainStart, "chain_end": modelChainEnd}
+        sanitised_PDBentries.append(entryToAppend)
 
-    return "blabla"
+        output = {
+            "sequenceLength": outputSequenceLength,
+            "sequence": outputSequence,
+            "PDBs": sanitised_PDBentries
+        }
+        
+        return output
+
+def import_uniprotID_list(path):
+    print(path)
+    
+    fields = []
+    output = []
+    with open(path, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        fields = next(csvreader)
+    
+        for row in csvreader:
+            # print(row)
+            zip_iterator = zip(fields, row)
+            zipped_dict = dict(zip_iterator)
+            # print(zipped_dict)
+            if not any(d.get('UniprotID', None) == zipped_dict["Uniprot ID"] for d in output):
+                entryToAppend = {"UniprotID": zipped_dict["Uniprot ID"], "glycosites": [int(zipped_dict["N-glycosylation site"])]}
+                output.append(entryToAppend)
+            
+            if any(d.get('UniprotID', None) == zipped_dict["Uniprot ID"] for d in output):
+                index = next((i for i, item in enumerate(output) if item["UniprotID"] == zipped_dict["Uniprot ID"]), None)
+                if int(zipped_dict["N-glycosylation site"]) not in output[index]["glycosites"]:
+                    output[index]["glycosites"].append(int(zipped_dict["N-glycosylation site"]))
+            
+    return output
+            
+
 
 
 if __name__ == "__main__":
     fileLocation = os.path.abspath(__file__)
-    uniprotID = "P07602"
-    query_uniprotID(uniprotID=uniprotID)
+    nDirectoriesToGoBack = 1
+    projectDir = os.path.normpath(
+        os.path.join(*([fileLocation] + [".."] * nDirectoriesToGoBack))
+    )
+    inputPathFileLocation = os.path.join(projectDir, "input/uniprotIDs.csv")
+    outputFolderLocation = os.path.join(projectDir, "output")
+    uniprotList = import_uniprotID_list(inputPathFileLocation)
+    
+    # get_pdbs_associated_with_uniprotid
+    
+    
+    
+    # uniprotID = "P07602"
+    # query = query_uniprotID(uniprotID=uniprotID)
+    
